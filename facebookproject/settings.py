9d08986b2f0e3d90b56897c73e451151b37b8987
mrishan <richududzlll@gmail.com>
2019-10-30 13:13:24 +0530

    initial commit

--- facebookproject/settings.py
+++ facebookproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'facebookproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
